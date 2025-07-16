3576576f65d5f0e1638e6cfbeb13104f2443a57e
Pramendra <pandeypramendra.as400@gmail.com>
2025-07-16 07:11:41 +0530

    added pages

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from jinja2.sandbox import unsafe
 from streamlit import session_state
 
 import funtions
@@ -9,9 +10,19 @@ def add_todo():
     todos.append(todo)
     funtions.write_todos(todos)
 
-st.title("my web-app")
+#st.title("my web-app to increase <b> productivity </b>",
+#         unsafe_allow_url=True)
+
+st.markdown("my web-app to increase <b>productivity</b>", unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Enter the todo ",
+              on_change=add_todo, key='new_todo')
+
 
 todos=funtions.get_todos()
+
+st.set_page_config(layout="wide")
+
 for index,todo in enumerate(todos):
     checkbox=st.checkbox(todo,key=todo)
     if checkbox:
@@ -20,7 +31,6 @@ for index,todo in enumerate(todos):
         del session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="Enter the todo ",
-              on_change=add_todo, key='new_todo')
+
 
 
